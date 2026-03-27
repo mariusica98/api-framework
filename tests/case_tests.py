7257f20e71c86691fd64096bc9ee8b8f4146448c
Marius Gaitan <m.gaitan@dev.asc.de>
2026-03-27 14:27:18 +0200

    Refcatoring

--- tests/case_tests.py
+++ tests/case_tests.py
@@ -1,13 +1,13 @@
-# tests/case_tests.py
-
 import pytest
 from utils.graphql_client import GraphQLClient
 from queries.case_queries import CREATE_CASE_MUTATION
 from variables.case_variables import create_case_input
 
+
 @pytest.fixture(scope="module")
 def client():
-    return GraphQLClient(env_name="T03")
+    return GraphQLClient()   # folosește implicit env_name="T03"
+
 
 def test_create_case(client):
     response = client.execute(CREATE_CASE_MUTATION, create_case_input)
