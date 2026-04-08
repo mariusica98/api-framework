f701153b73de8817e7e320613ea70b8aa5d18cbd
Marius Gaitan <m.gaitan@dev.asc.de>
2026-04-08 11:24:47 +0300

    Refactoring

--- utils/report_helper.py
+++ utils/report_helper.py
@@ -4,7 +4,6 @@ from variables.report_variables import *
 class ReportHelper:
     """
     Helper class for managing Reports via GraphQL.
-    Includes create operation for now.
     """
     def __init__(self, client):
         self.client = client
@@ -16,21 +15,3 @@ class ReportHelper:
         """
         response = self.client.execute(CREATE_REPORT_MUTATION, input_data)
         return response["data"]["createReport"]
-
-    def delete_report_by_id(self, report_id):
-        """
-        Deletes a report by its ID.
-        """
-        if not report_id:
-            return "skipped", None
-
-        variables = build_delete_report_by_id_input(report_id=report_id)
-        response = self.client.execute(DELETE_REPORT_BY_ID_MUTATION, variables)
-        result = response["data"].get("deleteReport")
-
-        if result is None or "id" not in result:
-            raise Exception(f"Failed to delete report {report_id}, response: {response}")
-
-        deleted_id = result["id"]
-        typename = result.get("__typename")
-        return deleted_id, typename
